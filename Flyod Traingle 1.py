5c7309451aa9c46159f7eff5b7399c735ddf0b7c
zaeemajmal <z@eem@jmal@123>
2026-02-12 17:37:45 +0530

    Star

--- Flyod Traingle 1.py
+++ Flyod Traingle 1.py
@@ -1,12 +1,8 @@
 rows = int(input("Enter the number of rows- "))
 n= 1
-#for i in range(rows):
-    #for j in range(i+1):
-        #print(i+1,end=" ")
-    #print("")
-
 for i in range(rows):
     for j in range(i+1):
         print(j+1,end=" ")
+        #print(i+1,end=" ")
     print("")    
         
---
+++ Star.py
@@ -0,0 +1,8 @@
+import turtle
+turtle.Screen().bgcolor("red")
+turtle.Screen().setup(300,400)
+star = turtle.Turtle()
+for i in range(5):
+     star.forward(200)
+     star.right(144)
+turtle.done()     
