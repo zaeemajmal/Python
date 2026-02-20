4227752b35e49a43b0bd000f818b8992771bb248
zaeemajmal <z@eem@jmal@123>
2026-02-20 16:42:08 +0530

    Shut down or restart

--- shutdown or restart.py
+++ shutdown or restart.py
@@ -1,6 +1,11 @@
 import os
-answer = input("Do you want to restart the computer or shutdown it- ")
-if answer == "shutdown" or answer == "Shutdown":
-  os.system("shutdown /s /t 1")
-if answer == "restart" or answer == "Restart":
-  os.system("shutdown /r /t 1")  
+def lock(a):
+  if a == "shutdown" or a == "Shutdown":
+   print("Shutting down please wait...")
+   os.system("shutdown /s /t 1")
+  elif a == "restart" or a == "Restart":
+   print("Restarting please wait...")
+   os.system("shutdown /r /t 1")  
+answer = input("Do you want to restart the computer or shutdown it- ")   
+lock(answer)   
+
