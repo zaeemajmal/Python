5d247a39f32f433f43eaff72c47a9d941a557e04
zaeemajmal <z@eem@jmal@123>
2026-03-10 17:32:09 +0530

    intersection

---
+++ iintersection.py
@@ -0,0 +1,11 @@
+football = {"Kanz","Kaleem","M.Humaid"}
+badminton = {"Kaleem","Kanz","Zaeem"}
+print(football.intersection(badminton))
+print(football&badminton)
+print(football.union(badminton))
+print(football|badminton)
+print(football.difference(badminton))
+print(badminton.difference(football))
+print(badminton-football)
+print(football.symmetric_difference(badminton))
+print(football^badminton)
