0430c82611d30f0fa6ac12b884db91dbde984287
zaeemajmal <z@eem@jmal@123>
2026-03-11 16:52:45 +0530

    set

---
+++ Set.py
@@ -0,0 +1,7 @@
+
+cricket = {"Student A","Student B","Student C","Student E"}
+football = {"Student C","Student F","Student A","Student E"}
+Basketball = {"Student D","Student E","Student B"}
+print("The students in all sports are: ",football.intersection(cricket,Basketball))
+print("The students in cricket are: ",cricket)
+print("The students in football and cricket are: ",football.intersection(cricket))
