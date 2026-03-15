eb650c35f17aef958217559ead16d9c344baade9
zaeemajmal <z@eem@jmal@123>
2026-03-15 17:19:52 +0530

    exit

---
+++ exit.py
@@ -0,0 +1,7 @@
+i = 1
+while i <= 10:
+    if i == 5:
+        exit()
+    print(i)
+    i+=1
+   
---
+++ zip.py
@@ -0,0 +1,9 @@
+n = ["A","B","C"]
+r = ["2","1","3"]
+m = [49,48,34]
+zipped =zip(n,r,m)
+print(list(zipped))
+l = [1,3,5,7,9]
+dict = {x:x*x for x in l}
+#{key:value for item in iterable}
+print(dict)
